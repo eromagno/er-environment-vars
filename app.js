3a5aa9a078ddf08bdd8ea258f281eee7d749df5d
Eugenio Romagnoli <eromagno@hotmail.com>
2023-05-02 12:28:02 -0400

    commit inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+import eventRoutes from './routes/events.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(eventRoutes);
+
+// PORT where to listen
+app.listen(process.env.PORT);
